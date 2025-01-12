9ca01ff370aa979bcf63191144c3ec1b06c83ca4
oriolorriols <115787579+oriolorriols@users.noreply.github.com>
2025-01-12 20:41:16 +0100

    Spinner when load

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,10 +3,8 @@ import "./header.scss";
 function Header() {
     return(
         <div className="nav">
-        <nav className="container mx-auto py-8">
-
+        <nav className="container ml-24 py-8">
             <h1>To Do List</h1>
-
         </nav>
         </div>
     )
